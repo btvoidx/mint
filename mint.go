ba580b8bb0b63c046ae602ffc55aa9c1175e6bfc
Max Tumozov <maksimtumozov@gmail.com>
2023-04-27 23:04:11 +0400

    revert hooks to always use `any`

--- mint.go
+++ mint.go
@@ -9,16 +9,11 @@ import (
 	"sync"
 )
 
-// Plugin is a function that takes Emitted values of type T
-// and returns nil or a function that will be called after
-// all consumers got the Emitted value.
-type Plugin[T any] func(T) func()
-
 // Emitter holds all active consumers and Emit hooks.
 type Emitter struct {
 	subc    uint64
 	subs    map[uint64]any // func(T)
-	plugins []any          // Plugin
+	plugins []func(any) func()
 
 	mu   sync.Mutex
 	once sync.Once
@@ -28,15 +23,13 @@ func (e *Emitter) init() {
 	e.once.Do(func() { e.subs = make(map[uint64]any) })
 }
 
-// Sequentially pushes value v to all consumers. Order in which consumers
+// Sequentially pushes value v to all consumers of type T. Order in which consumers
 // receive the value is not determenistic.
 func Emit[T any](e *Emitter, v T) {
 	for _, fn := range e.plugins {
-		if fn, ok := fn.(Plugin[T]); ok {
-			after := fn(v)
-			if after != nil {
-				defer after()
-			}
+		after := fn(v)
+		if after != nil {
+			defer after()
 		}
 	}
 
@@ -50,8 +43,9 @@ func Emit[T any](e *Emitter, v T) {
 	}
 }
 
-// Registers a new consumer. If T is an interface, fn is called with all values which implement T.
-// So if T is any, fn will receive any emitted value.
+// Registers a new consumer that receives all values which were
+// emitted as T. So that On(e, func(any)) will receive all values
+// emitted with Emit[any](e, ...)
 //
 // Calling off stops consumer. Multiple calls are no-op.
 func On[T any](e *Emitter, fn func(T)) (off func()) {
@@ -75,10 +69,10 @@ func On[T any](e *Emitter, fn func(T)) (off func()) {
 
 // Use allows to hook into event emitting process. Plugns are
 // called sequentially in order they were added to Emitter.
-// Plugin is a function that takes Emitted values of type T
-// and returns nil or a function that will be called after
+// Plugin is a function that takes Emitted values and
+// returns nil or a function that will be called after
 // all consumers got the Emitted value. Returned functions
 // are called in reverse order.
-func Use[T any](e *Emitter, p Plugin[T]) {
-	e.plugins = append(e.plugins, p)
+func Use(e *Emitter, plugin func(any) func()) {
+	e.plugins = append(e.plugins, plugin)
 }
